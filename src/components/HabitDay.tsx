50fffe751c5569c1c526dea45391408898e5a442
diegojfcampos <diegojfcampos@gmail.com>
2023-05-12 03:46:54 +0100

    feat: adding habits per week days

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from './ProgressBar';
 import clsx from 'clsx';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check } from 'phosphor-react';
 
 
 interface HabitDayProps {
@@ -28,7 +30,20 @@ export function HabitDay({completed, amount}: HabitDayProps) {
                 <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flec flex-col">
                     <span className="font-semibold text-zinc-400">Tuesday</span>
                     <span className="mt-1 font-extrabold leading-tight text-3xl">02/05</span>
+
                     <ProgressBar progress={completePercentage}/>
+
+                    <div className="mt-6 flex flex-col gap-3">
+                        <Checkbox.Root className="flex  items-center gap-3 group">
+                            <div className="w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                                <Checkbox.Indicator>
+                                    <Check size={20} className="text-white" />
+                                </Checkbox.Indicator>
+                            </div>
+                            
+                            <span className="font-seibold text-2xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">Drink 2l water</span>
+                        </Checkbox.Root>
+                    </div>
                    
                     <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
                 </Popover.Content>
