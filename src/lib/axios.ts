b4c570f108cbc009bd13e78e45458d3222382e88
diegojfcampos <diegojfcampos@gmail.com>
2023-06-13 02:49:44 +0100

    add: headers to axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
     baseURL: 'https://api-habit-helper.vercel.app/api/v1',
+    headers: {
+        'Content-Type': 'application/json',
+      },
 })
