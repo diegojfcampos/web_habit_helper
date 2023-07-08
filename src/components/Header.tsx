fa4474c2a955bf457778dccc336fe8629dd6f540
diegojfcampos <diegojfcampos@gmail.com>
2023-07-09 00:47:06 +0100

    refact: modal colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export function Header(){
                 type="button"                 
                 className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
             >        
-            <Plus size={20} className="text-violet- font-semibold text-lg"/>
+            <Plus size={20} className="text-violet- font-bold text-8xl"/>
                 New Habit
             </Dialog.Trigger >
             <Dialog.Portal>
