80b40b5b49f2ee4c310bbded3e96d77d01560124
diegojfcampos <diegojfcampos@gmail.com>
2023-07-11 09:14:16 +0100

    feat: summary table horizontal scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
-// import {Habit} from '../src/components/Habits'
+import * as ScrollArea from '@radix-ui/react-scroll-area';
 
 export function App() {
     return (  
     <div className="w-screen h-screen flex justify-center items-center">   
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         
-       <Header/>
-       <SummaryTable />
+       <Header/>      
+
+       <ScrollArea.Root>
+        <ScrollArea.Viewport />
+        <ScrollArea.Scrollbar orientation="horizontal">
+          <SummaryTable />
+          <ScrollArea.Thumb />
+        </ScrollArea.Scrollbar>
+        <ScrollArea.Corner />
+      </ScrollArea.Root>
 
       </div>
     </div>
--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -4,6 +4,7 @@ import { HabitDay } from "./HabitDay";
 import {api} from "../lib/axios"
 import dayjs from "dayjs";
 
+
 const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
 const summaryDates = generateYearDates()
 const minimunSummaryDaySize = 18 * 7;
@@ -28,38 +29,39 @@ export function SummaryTable(){
         })
     }, [])
     
-    return(
-      <div className="w-full flex">
-        <div className="grid grid-rows-7 grid-flow-row gap-3">
-            {weekDays.map((weekDay, index) =>{
-                return(
-                    <div key={`${weekDay}-${index}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">{weekDay}</div>
-                )
-            })}        
-        </div>
+        return(       
 
-        <div className="grid grid-rows-7 grid-flow-col gap-3">            
-            {summary.length > 0 && summaryDates.map(date => {
-                const dayInSummary = summary.find(day => {
-                    const formattedDate = dayjs(date).format('YYYY-MM-DD');
-                    return dayjs(formattedDate).isSame(day.date, 'day')
-                })
-                return (                    
-                    <HabitDay
-                        key={date.toString()}
-                        date={date}
-                        amount={dayInSummary?.amount}  
-                        defaultcompleted={dayInSummary?.completed}
-                    />
-                )
-            })}
+          <div className="w-full flex">
+            <div className="grid grid-rows-7 grid-flow-row gap-3">
+                {weekDays.map((weekDay, index) =>{
+                    return(
+                        <div key={`${weekDay}-${index}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">{weekDay}</div>
+                    )
+                })}        
+            </div>
 
-            {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
-                return <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+            <div className="grid grid-rows-7 grid-flow-col gap-3">            
+                {summary.length > 0 && summaryDates.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        const formattedDate = dayjs(date).format('YYYY-MM-DD');
+                        return dayjs(formattedDate).isSame(day.date, 'day')
+                    })
+                    return (                    
+                        <HabitDay
+                            key={date.toString()}
+                            date={date}
+                            amount={dayInSummary?.amount}  
+                            defaultcompleted={dayInSummary?.completed}
+                        />
+                    )
+                })}
 
-            })}
-        </div>
+                {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
+                    return <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
 
-      </div> 
-    )
-}
+                })}
+            </div>
+
+          </div>         
+        )
+}   
